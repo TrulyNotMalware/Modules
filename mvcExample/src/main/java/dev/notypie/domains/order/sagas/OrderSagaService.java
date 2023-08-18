fea178c484455456d3c8542d50803f6c3fc6b872
TrulyNotMalware <notleebutyee@gmail.com>
2023-08-18 10:52:04 +0900

    fix : Import configurations

--- mvcExample/src/main/java/dev/notypie/domains/order/sagas/OrderSagaService.java
+++ mvcExample/src/main/java/dev/notypie/domains/order/sagas/OrderSagaService.java
@@ -4,13 +4,30 @@ import dev.notypie.domains.order.domain.Order;
 import dev.notypie.domains.order.domain.OrderRepository;
 import dev.notypie.domains.order.messaging.common.OrderDetails;
 import io.eventuate.tram.sagas.orchestration.SagaInstanceFactory;
+import io.eventuate.tram.sagas.spring.orchestration.SagaOrchestratorConfiguration;
+import io.eventuate.tram.spring.commands.common.TramCommandsCommonAutoConfiguration;
+import io.eventuate.tram.spring.commands.producer.TramCommandProducerConfiguration;
+import io.eventuate.tram.spring.consumer.common.TramConsumerBaseCommonConfiguration;
+import io.eventuate.tram.spring.consumer.jdbc.TramConsumerJdbcAutoConfiguration;
+import io.eventuate.tram.spring.consumer.kafka.EventuateTramKafkaMessageConsumerConfiguration;
+import io.eventuate.tram.spring.messaging.common.TramMessagingCommonAutoConfiguration;
+import io.eventuate.tram.spring.messaging.producer.common.TramMessagingCommonProducerConfiguration;
+import io.eventuate.tram.spring.messaging.producer.jdbc.TramMessageProducerJdbcConfiguration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 //Instead of explicitly @Import-ing configuration classes you can rely on the auto-configuration provided by eventuate-tram-sagas-spring-orchestration-simple-dsl-starter
 @Service
 @RequiredArgsConstructor
+@Import({SagaOrchestratorConfiguration.class,//SagaInstanceFactory
+        TramMessagingCommonAutoConfiguration.class,//ChannelMapping
+        TramMessageProducerJdbcConfiguration.class,//MessageProducerImplementation
+        TramCommandsCommonAutoConfiguration.class,//CommandNameMapping
+        EventuateTramKafkaMessageConsumerConfiguration.class,//MessageConsumer
+        TramConsumerJdbcAutoConfiguration.class//DuplicateMessageDetector
+})
 public class OrderSagaService {
     private final OrderRepository orderRepository;
     private final SagaInstanceFactory sagaInstanceFactory;
@@ -18,6 +35,7 @@ public class OrderSagaService {
 
     @Transactional
     public Order createOrder(OrderDetails orderDetails){
+
         CreateOrderSagaData data = new CreateOrderSagaData(orderDetails);
         this.sagaInstanceFactory.create(createOrderSaga, data);
         return this.orderRepository.findById(data.getOrderId()).get();
