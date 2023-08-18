97f886ff7249ce12d34579956648ef2c4c9d3f6a
TrulyNotMalware <notleebutyee@gmail.com>
2023-08-18 17:56:59 +0900

    feat : Add example controller

--- mvcExample/src/main/java/dev/notypie/MvcExampleApplication.java
+++ mvcExample/src/main/java/dev/notypie/MvcExampleApplication.java
@@ -2,10 +2,7 @@ package dev.notypie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = {"dev.notypie.domains.order.domain"})
 @SpringBootApplication
 public class MvcExampleApplication {
     public static void main(String[] args) {
--- mvcExample/src/main/java/dev/notypie/domains/order/domain/OrderRepository.java
+++ mvcExample/src/main/java/dev/notypie/domains/order/domain/OrderRepository.java
@@ -3,6 +3,10 @@ package dev.notypie.domains.order.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findAllByOrderDetailsCustomerId(Long customerId);
 }
