d3ef411fa8d8ddc5fea0e87c06e35666d876fd98
TrulyNotMalware <notleebutyee@gmail.com>
2024-03-08 18:09:22 +0900

    refact : Auth DDD update

--- auth/src/main/java/dev/notypie/infrastructure/dao/user/memory/KeyValueConfiguration.java
+++ auth/src/main/java/dev/notypie/infrastructure/dao/user/memory/KeyValueConfiguration.java
@@ -4,13 +4,17 @@ import dev.notypie.aggregate.user.repository.UserDomainRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @ConditionalOnProperty(prefix = "auth.app.persistent", name = "type", havingValue = "memory", matchIfMissing = true)
 public class KeyValueConfiguration {
 
     @Bean
-    public UserDomainRepository userDomainRepository(){
-        return new KeyValueUserDomainRepositoryImpl();
+    public UserDomainRepository userDomainRepository(
+            PasswordEncoder passwordEncoder
+    ){
+        return new KeyValueUserDomainRepositoryImpl(passwordEncoder);
     }
 }
--- auth/src/main/java/dev/notypie/infrastructure/dao/user/memory/KeyValueUserDomainRepositoryImpl.java
+++ auth/src/main/java/dev/notypie/infrastructure/dao/user/memory/KeyValueUserDomainRepositoryImpl.java
@@ -3,15 +3,21 @@ package dev.notypie.infrastructure.dao.user.memory;
 import dev.notypie.aggregate.user.entity.User;
 import dev.notypie.aggregate.user.repository.UserDomainRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
 public class KeyValueUserDomainRepositoryImpl implements UserDomainRepository {
+    private final PasswordEncoder passwordEncoder;
 
+    //FIXME when successfully authenticated, refreshTokenService.isDuplicateRefreshToken(id) throw exception cause there's no database.
     private final Map<Long, User> database = new ConcurrentHashMap<>();
 
+    KeyValueUserDomainRepositoryImpl(PasswordEncoder passwordEncoder){
+        this.passwordEncoder=passwordEncoder;
+    }
 
     @Override
     public User findById(Long id) {
@@ -27,6 +33,13 @@ public class KeyValueUserDomainRepositoryImpl implements UserDomainRepository {
 
     @Override
     public User save(User user) {
-        return this.database.put(user.getId(), user);
+        encryptPassword(user);
+        this.database.put(user.getId(), user);
+        return this.database.get(user.getId());
+    }
+
+    private void encryptPassword(User user){
+        String password = user.getUserInformation().getPassword();
+        user.changePassword(this.passwordEncoder.encode(password));
     }
 }
