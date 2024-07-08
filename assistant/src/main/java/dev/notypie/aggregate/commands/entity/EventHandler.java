1e4a1482ca36aeb19b7f5749c7889b52c2fffcf7
TrulyNotMalware <notleebutyee@gmail.com>
2024-07-08 17:17:18 +0900

    remove : slack related packages

--- assistant/src/main/java/dev/notypie/aggregate/commands/entity/EventHandler.java
+++
@@ -1,9 +0,0 @@
-package dev.notypie.aggregate.commands.entity;
-
-import org.springframework.http.ResponseEntity;
-
-@Deprecated( forRemoval = true )
-public interface EventHandler<INPUT, OUTPUT> {
-
-    ResponseEntity<OUTPUT> generateEventResponse(INPUT event);
-}
--- assistant/src/main/java/dev/notypie/aggregate/commands/repository/CommandRepository.java
+++
@@ -1,5 +0,0 @@
-package dev.notypie.aggregate.commands.repository;
-
-@Deprecated( forRemoval = true )
-public interface CommandRepository {
-}
